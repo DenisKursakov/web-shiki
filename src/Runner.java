929f88db3286d8f9f6b105eba17291cb0f254dce
DenisKursakov <itisfunny4x@gmail.com>
2021-11-23 18:01:58 +0300

    second commit

--- src/Runner.java
+++ src/Runner.java
@@ -15,25 +15,25 @@ public class Runner {
         final String FIRST_WORD = "result(";
         try (Scanner scanner = new Scanner(new FileReader(INPUT_CSV))) {
             scanner.useLocale(Locale.ENGLISH);
-            while (scanner.hasNext()){
+            while (scanner.hasNext()) {
                 String LINE = scanner.nextLine();
                 String[] st = LINE.split(SEMICOLON);
                 try {
                     String element = st[Integer.parseInt(st[0])];
                     double currentElement = Double.parseDouble(element);
                     sumOfElements += currentElement;
-                    if(result.equals("result(")){
+                    if (result.equals("result(")) {
                         result += currentElement;
                         continue;
                     }
-                    if(!element.contains("-")){
+                    if (!element.contains("-")) {
                         result += PLUS + currentElement;
                     } else {
                         element = MINUS + String.valueOf(currentElement).substring(1);
                         result += element;
                     }
 
-                } catch ( IndexOutOfBoundsException | NumberFormatException e0) {
+                } catch (IndexOutOfBoundsException | NumberFormatException e0) {
                     errorLinesCount++;
                 }
             }
@@ -41,6 +41,6 @@ public class Runner {
         } catch (FileNotFoundException e1) {
             System.out.println("File is not found");
         }
-        System.out.printf("%s) = %.2f\nerror-lines = %d",result,sumOfElements,errorLinesCount);
+        System.out.printf("%s) = %.2f\nerror-lines = %d", result, sumOfElements, errorLinesCount);
     }
 }
