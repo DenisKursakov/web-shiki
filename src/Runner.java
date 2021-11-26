5f9645cd4af46cc80e0d7ea4770e56032a83e6bf
DenisKursakov <84324765+DenisKursakov@users.noreply.github.com>
2021-11-26 11:42:55 +0300

    fourth commit

--- src/Runner.java
+++ src/Runner.java
@@ -8,18 +8,21 @@ public class Runner {
         double sumOfElements = 0;
         int errorLinesCount = 0;
         final String INPUT_CSV = "src/in.csv";
-        final String PLUS = " + ";
-        final String MINUS_WITH_SPACES = " - ";
-        final String MINUS = "-";
+        final String BEFORE_SIGN = " ";
+        final String AFTER_SIGN = " ";
+        final String MINUS_WITHOUT_SPACES = "-";
+        final String PLUS = BEFORE_SIGN + "+" + AFTER_SIGN;
+        final String MINUS = BEFORE_SIGN + MINUS_WITHOUT_SPACES + AFTER_SIGN;
         final String SEMICOLON = ";";
-        final String EQUAL_SIGN = " = ";
+        final String EQUAL_SIGN = BEFORE_SIGN + "=" + AFTER_SIGN;
         final String LEFT_BRACKET = "(";
         final String RIGHT_BRACKET = ")";
         final String FIRST_WORD = "result" + LEFT_BRACKET;
-        final String ERROR_LINES = "error" + MINUS + "lines";
+        final String RESULT_TAIL = RIGHT_BRACKET + EQUAL_SIGN;
+        final String ERROR_LINES = "error" + MINUS_WITHOUT_SPACES + "lines" + EQUAL_SIGN;
         final String FILE_IS_NOT_FOUND = "File is not found";
         final String TABULATION = "\n";
-        String result = FIRST_WORD;
+        StringBuilder strResult = new StringBuilder(FIRST_WORD);
         try (Scanner scanner = new Scanner(new FileReader(INPUT_CSV))) {
             scanner.useLocale(Locale.ENGLISH);
             while (scanner.hasNext()) {
@@ -29,15 +32,15 @@ public class Runner {
                     String element = stringElements[Integer.parseInt(stringElements[0])];
                     double currentElement = Double.parseDouble(element);
                     sumOfElements += currentElement;
-                    if (result.equals(FIRST_WORD)) {
-                        result += currentElement;
+                    if (strResult.toString().equals(FIRST_WORD)) {
+                        strResult.append(currentElement);
                         continue;
                     }
-                    if (!element.contains(MINUS)) {
-                        result += PLUS + currentElement;
+                    if (!element.contains(MINUS_WITHOUT_SPACES)) {
+                        strResult.append(PLUS).append(currentElement);
                     } else {
-                        element = MINUS_WITH_SPACES + String.valueOf(currentElement).substring(1);
-                        result += element;
+                        element = MINUS + String.valueOf(currentElement).substring(1);
+                        strResult.append(element);
                     }
 
                 } catch (IndexOutOfBoundsException | NumberFormatException e0) {
@@ -48,7 +51,7 @@ public class Runner {
         } catch (FileNotFoundException e1) {
             System.out.println(FILE_IS_NOT_FOUND);
         }
-        System.out.println(result + RIGHT_BRACKET + EQUAL_SIGN + sumOfElements +
-                TABULATION + ERROR_LINES + EQUAL_SIGN + errorLinesCount);
+        System.out.println(strResult + RESULT_TAIL + sumOfElements +
+                TABULATION + ERROR_LINES + errorLinesCount);
     }
 }
