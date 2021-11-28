6b9e4e33811787e79fd8c9a168f472ce349040fd
DenisKursakov <84324765+DenisKursakov@users.noreply.github.com>
2021-11-28 18:58:55 +0300

    initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Runner {
+    public static void main(String[] args) {
+        final String INDEX = "index";
+        final String VALUE = "value";
+        final String AFTER_SIGN = " ";
+        final String BEFORE_SIGN = " ";
+        final String EQUAL_SIGN = AFTER_SIGN + "=" + BEFORE_SIGN;
+        final String ERROR_LINES = "error-lines";
+        final String SUM = "sum";
+        final String KEY_VALUE_REGEX = "[1-9](\\d*)";
+        int errorLineCount = 0;
+        ResourceBundle rb = ResourceBundle.getBundle("in", Locale.ENGLISH);
+        Enumeration<String> keys = rb.getKeys();
+        String key;
+        double sum = 0.0;
+        while (keys.hasMoreElements()) {
+            key = keys.nextElement();
+            try {
+                if (key.matches(INDEX + KEY_VALUE_REGEX) &&
+                        rb.getString(key).trim().matches(KEY_VALUE_REGEX)) {
+                    String i = key.substring(INDEX.length());
+                    String j = rb.getString(key);
+                    sum += Double.parseDouble(rb.getString(VALUE + i + j));
+                }
+            } catch (IndexOutOfBoundsException | NumberFormatException | MissingResourceException e) {
+                errorLineCount++;
+            }
+        }
+        System.out.println(SUM + EQUAL_SIGN + sum);
+        System.out.println(ERROR_LINES + EQUAL_SIGN + errorLineCount);
+    }
+}
