1341ccc7c036f49f63da0d6be87856d076642d4c
DenisKursakov <84324765+DenisKursakov@users.noreply.github.com>
2021-11-29 15:40:08 +0300

    third commit

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,40 +1,52 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Runner {
     public static void main(String[] args) {
-        final String INDEX = "index";
         final String VALUE = "value";
         final String AFTER_SIGN = " ";
         final String BEFORE_SIGN = " ";
+        final int TAIL_INDEX = 1;
         final String EQUAL_SIGN = AFTER_SIGN + "=" + BEFORE_SIGN;
         final String ERROR_LINES = "error-lines";
         final String SUM = "sum";
-        final String BASE_NAME = "in";
+        final String MISSING_RESOURCE_EXC = "Missing resource exception";
+        final String BASE_NAME = "in1";
         final String KEY_VALUE_REGEX = "[1-9](\\d*)";
-        final String NUMBER_REGEX = "(.*)";
+        final String INDEX_REGEX = "index(.*)";
         int errorLineCount = 0;
-        ResourceBundle rb = ResourceBundle.getBundle(BASE_NAME, Locale.ENGLISH);
-        Enumeration<String> keys = rb.getKeys();
-        String key;
-        double sum = 0.0;
-        while (keys.hasMoreElements()) {
-            key = keys.nextElement();
-            if (key.matches(INDEX + NUMBER_REGEX)) {
-                try {
-                    if (key.matches(INDEX + KEY_VALUE_REGEX) &&
-                            rb.getString(key).trim().matches(KEY_VALUE_REGEX)) {
-                        String i = key.substring(INDEX.length());
-                        String j = rb.getString(key);
-                        sum += Double.parseDouble(rb.getString(VALUE + i + j));
+        try {
+            ResourceBundle rb = ResourceBundle.getBundle(BASE_NAME, Locale.ENGLISH);
+            Enumeration<String> keys = rb.getKeys();
+            String key;
+            double sum = 0.0;
+            while (keys.hasMoreElements()) {
+                key = keys.nextElement();
+                Pattern pattern = Pattern.compile(INDEX_REGEX);
+                Matcher keyMatcher = pattern.matcher(key);
+                if (keyMatcher.matches()) {
+                    String i = keyMatcher.group(TAIL_INDEX);
+                    String j = rb.getString(key).trim();
+                    Pattern patternIJ = Pattern.compile(KEY_VALUE_REGEX);
+                    Matcher iMatcher = patternIJ.matcher(i);
+                    Matcher jMatcher = patternIJ.matcher(j);
+                    if (iMatcher.matches() && jMatcher.matches()) {
+                        String valueIJ = VALUE + i + j;
+                        try {
+                            sum += Double.parseDouble(rb.getString(valueIJ));
+                        } catch (NumberFormatException | MissingResourceException e) {
+                            errorLineCount++;
+                        }
                     } else {
                         errorLineCount++;
                     }
-                } catch (NumberFormatException | MissingResourceException e) {
-                    errorLineCount++;
                 }
             }
+            System.out.println(SUM + EQUAL_SIGN + sum);
+            System.out.println(ERROR_LINES + EQUAL_SIGN + errorLineCount);
+        } catch (MissingResourceException e1) {
+            System.out.println(MISSING_RESOURCE_EXC);
         }
-        System.out.println(SUM + EQUAL_SIGN + sum);
-        System.out.println(ERROR_LINES + EQUAL_SIGN + errorLineCount);
     }
 }
---
+++ src/test/java/RunnerTest.java
@@ -0,0 +1,87 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RunnerTest {
+    private static int getResult(String baseName, StringBuilder strResult) throws FileNotFoundException {
+        final String VALUE = "value";
+        final String AFTER_SIGN = " ";
+        final String BEFORE_SIGN = " ";
+        final int TAIL_INDEX = 1;
+        final String EQUAL_SIGN = AFTER_SIGN + "=" + BEFORE_SIGN;
+        final String SUM = "sum";
+//      final String BASE_NAME = "in1";
+        final String KEY_VALUE_REGEX = "[1-9](\\d*)";
+        final String INDEX_REGEX = "index(.*)";
+        int errorLineCount = 0;
+        ResourceBundle rb = ResourceBundle.getBundle(baseName, Locale.ENGLISH);
+        Enumeration<String> keys = rb.getKeys();
+        String key;
+        double sum = 0.0;
+        while (keys.hasMoreElements()) {
+            key = keys.nextElement();
+            Pattern pattern = Pattern.compile(INDEX_REGEX);
+            Matcher keyMatcher = pattern.matcher(key);
+            if (keyMatcher.matches()) {
+                String i = keyMatcher.group(TAIL_INDEX);
+                String j = rb.getString(key).trim();
+                Pattern patternIJ = Pattern.compile(KEY_VALUE_REGEX);
+                Matcher iMatcher = patternIJ.matcher(i);
+                Matcher jMatcher = patternIJ.matcher(j);
+                if (iMatcher.matches() && jMatcher.matches()) {
+                    String valueIJ = VALUE + i + j;
+                    try {
+                        sum += Double.parseDouble(rb.getString(valueIJ));
+                    } catch (NumberFormatException | MissingResourceException e) {
+                        errorLineCount++;
+                    }
+                } else {
+                    errorLineCount++;
+                }
+            }
+        }
+        strResult = strResult.append(SUM + EQUAL_SIGN + sum);
+        return errorLineCount;
+    }
+
+    @Test
+    public void testMainScenarioWhenBaseNameIsIn1() throws FileNotFoundException {
+        StringBuilder result = new StringBuilder();
+        int errorLines = getResult("in1", result);
+        int expectedNine = 9;
+        Assert.assertEquals(expectedNine, errorLines);
+        String expectedResultIn1 = "sum = 30.242";
+        Assert.assertEquals(expectedResultIn1, result.toString());
+    }
+
+    @Test
+    public void testMainScenarioWhenBaseNameIsIn2() throws FileNotFoundException {
+        StringBuilder result = new StringBuilder();
+        int errorLines = getResult("in2", result);
+        int expectedNine = 3;
+        Assert.assertEquals(expectedNine, errorLines);
+        String expectedResultIn1 = "sum = 8.24";
+        Assert.assertEquals(expectedResultIn1, result.toString());
+    }
+
+    @Test(expected = MissingResourceException.class)
+    public void testWrongBaseNameWhenBaseNameIsIn1() throws FileNotFoundException {
+        StringBuilder result = new StringBuilder();
+        String baseName = "in5";
+        getResult(baseName, result);
+    }
+
+    @Test(expected = MissingResourceException.class)
+    public void testWrongBaseNameWhenBaseNameIsIn2() throws FileNotFoundException {
+        StringBuilder result = new StringBuilder();
+        String baseName = "in4";
+        getResult(baseName, result);
+    }
+}
