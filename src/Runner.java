7b5827e036940f8b896b1dc997d49f7a4655093c
DenisKursakov <84324765+DenisKursakov@users.noreply.github.com>
2022-01-05 15:47:07 +0300

    sixth commit

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,5 @@
 import by.epam.lab.comparators.NumComparator;
 import by.epam.lab.constants.Constants;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.*;
@@ -13,15 +12,15 @@ public class Runner {
                 String currentLine = scanner.nextLine();
                 String[] coordinates = currentLine.split(Constants.REGEX_FOR_COORDINATES);
                 int length = calculateLength(coordinates);
-                int num = numLenMap.get(length) != null ?
-                        numLenMap.get(length) + 1 : 1;
-                numLenMap.put(length, num);
+                Integer num = numLenMap.get(length);
+                if (num == null) {
+                    num = 0;
+                }
+                numLenMap.put(length, num + 1);
             }
-            List<Map.Entry<Integer, Integer>> segmentList = new ArrayList<>(numLenMap.entrySet());
-            Collections.sort(segmentList, new NumComparator());
-            for (Map.Entry<Integer, Integer> entry : segmentList) {
-                System.out.println(entry.getKey() + Constants.SEMICOLON + entry.getValue());
-            }
+            List<Map.Entry<Integer, Integer>> numLenList = new ArrayList<>(numLenMap.entrySet());
+            Collections.sort(numLenList, new NumComparator());
+            printCollection(numLenList);
         } catch (FileNotFoundException e) {
             System.out.println(Constants.FILE_IS_NOT_FOUND);
         }
@@ -33,5 +32,11 @@ public class Runner {
         double y1 = Double.parseDouble(coordinates[Constants.Y1_COORDINATE_ID]);
         double y2 = Double.parseDouble(coordinates[Constants.Y2_COORDINATE_ID]);
         return (int) Math.round(Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
+    }
+
+    private static void printCollection(List<Map.Entry<Integer, Integer>> numLenList) {
+        for (Map.Entry<Integer, Integer> entry : numLenList) {
+            System.out.println(entry.getKey() + Constants.SEMICOLON + entry.getValue());
+        }
     }
 }
