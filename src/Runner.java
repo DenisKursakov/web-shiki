1cec3037e80371a9dae499feee2d39e4ab18ed51
DenisKursakov <84324765+DenisKursakov@users.noreply.github.com>
2022-01-07 20:32:16 +0300

    first commit

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,10 @@
 import by.epam.lab.Constants;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Runner {
     public static void main(String[] args) {
@@ -17,18 +21,24 @@ public class Runner {
                 //create resultSet a with table after sorting
                 ResultSet resultCoordinatesSet = coordinatesStatement.executeQuery(
                         Constants.SELECT_COORDINATES_TABLE_AFTER_SORTING);
+                List<Integer[]> coordinatesList = new ArrayList<>();
+                while (resultCoordinatesSet.next()) {
+                    coordinatesList.add(new Integer[]{
+                            resultCoordinatesSet.getInt(Constants.LEN_ID_IN_COORDINATES_TABLE),
+                            resultCoordinatesSet.getInt(Constants.NUM_ID_IN_COORDINATE_TABLE)
+                    });
+                }
+
                 //create empty statement for working with the frequencies table
                 frequenciesStatement = cn.createStatement();
                 //delete all data from the table frequencies
                 frequenciesStatement.executeUpdate(Constants.TRUNCATE_FREQUENCIES_TABLE);
-                while (resultCoordinatesSet.next()) {
-                    //enter new data into the table frequencies using INSERT SQL query
+                //enter new data into the table frequencies using INSERT SQL query
+                for (Integer[] element : coordinatesList) {
                     frequenciesStatement.executeLargeUpdate(String.format(
-                            Constants.INSERT_INTO_FREQUENCIES_TABLE,
-                            resultCoordinatesSet.getInt(Constants.LEN_ID_IN_COORDINATES_TABLE),
-                            resultCoordinatesSet.getInt(Constants.NUM_ID_IN_COORDINATE_TABLE))
-                    );
+                            Constants.INSERT_INTO_FREQUENCIES_TABLE, element[0], element[1]));
                 }
+
                 //create a new result set with all data from frequencies table
                 ResultSet resultFrequenciesSet = frequenciesStatement.executeQuery(
                         Constants.SELECT_FREQUENCIES_TABLE);
@@ -46,11 +56,12 @@ public class Runner {
                 if (rs != null && !rs.isClosed()) {
                     rs.close();
                 }
-                if (coordinatesStatement != null) {
-                    coordinatesStatement.close();
-                }
                 if (frequenciesStatement != null) {
                     frequenciesStatement.close();
+                }
+
+                if (coordinatesStatement != null) {
+                    coordinatesStatement.close();
                 }
                 if (cn != null) {
                     cn.close();
