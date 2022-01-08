8508147cccb7c8ea92781d80c42f86ab2ee4b927
DenisKursakov <84324765+DenisKursakov@users.noreply.github.com>
2022-01-09 00:41:21 +0300

    third commit

--- src/Runner.java
+++ src/Runner.java
@@ -5,44 +5,42 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static by.epam.lab.Constants.*;
+
 public class Runner {
     public static void main(String[] args) {
-        try (Connection cn = DriverManager.getConnection(
-                Constants.URL, Constants.USER, Constants.PASSWORD);
+        try (Connection cn = DriverManager.getConnection(URL, USER, PASSWORD);
              Statement st = cn.createStatement();
              PreparedStatement ps =
-                     cn.prepareStatement(Constants.INSERT_INTO_FREQUENCIES_TABLE)) {
-            try (ResultSet rs = st.executeQuery(Constants.SELECT_COORDINATES_TABLE_AFTER_SORTING)) {
-                Class.forName(Constants.FOR_NAME_WAY);
+                     cn.prepareStatement(INSERT_INTO_FREQUENCIES_TABLE)) {
+            try (ResultSet rs = st.executeQuery(SELECT_COORDINATES_TABLE_AFTER_SORTING)) {
                 //create list with results from coordinates table after sorting
                 List<LenNum> lenNumList = new ArrayList<>();
                 while (rs.next()) {
-                    LenNum lenNum = new LenNum(rs.getInt(Constants.LEN_IND),
-                            rs.getInt(Constants.NUM_IND));
+                    LenNum lenNum = new LenNum(rs.getInt(LEN_ID_FOR_SET),
+                            rs.getInt(NUM_ID_FOR_SET));
                     lenNumList.add(lenNum);
                     System.out.println(lenNum);
                 }
                 //delete all data from the table frequencies
-                st.executeUpdate(Constants.DELETE_ALL_FROM_FREQUENCIES_TABLE);
+                st.executeUpdate(DELETE_ALL_FROM_FREQUENCIES_TABLE);
                 //enter new data into the table frequencies using INSERT SQL query
                 for (LenNum numLen : lenNumList) {
-                    ps.setInt(Constants.LEN_ID_FOR_SET,
-                            numLen.getSegmentsLength());
-                    ps.setInt(Constants.NUM_ID_FOR_SET,
-                            numLen.getNumberOfSegments());
+                    ps.setInt(LEN_ID_FOR_SET, numLen.getSegmentsLength());
+                    ps.setInt(NUM_ID_FOR_SET, numLen.getNumberOfSegments());
                     ps.addBatch();
                 }
+                ps.executeBatch();
             }
-            ps.executeBatch();
             //create a new result set with all data from frequencies table after sorting
-            try (ResultSet rs = st.executeQuery(Constants.SELECT_FREQUENCIES_TABLE_AFTER_SORTING)) {
+            try (ResultSet rs = st.executeQuery(SELECT_FREQUENCIES_TABLE_AFTER_SORTING)) {
                 //print num len data from frequencies table after sorting
                 while (rs.next()) {
-                    System.out.println(rs.getInt(Constants.LEN_ID_IN_FREQUENCIES_TABLE) +
-                            Constants.SEMICOLON + rs.getInt(Constants.NUM_ID_IN_FREQUENCIES_TABLE));
+                    System.out.println(rs.getInt(LEN_ID_IN_FREQUENCIES_TABLE) +
+                            SEMICOLON + rs.getInt(NUM_ID_IN_FREQUENCIES_TABLE));
                 }
             }
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e) {
             System.err.println(e);
         }
     }
