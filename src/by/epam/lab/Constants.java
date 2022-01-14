c1fdea5000193434ee4199a2e0d8eccba8ff8f06
DenisKursakov <84324765+DenisKursakov@users.noreply.github.com>
2022-01-14 13:27:49 +0300

    third commit

--- src/by/epam/lab/Constants.java
+++ src/by/epam/lab/Constants.java
@@ -4,6 +4,8 @@ public class Constants {
     public static final String WAY_RESULTS_XML = "src/results.xml";
     public static final String SEMICOLON = ";";
     public static final String FORMAT_MARK = "%d.%d";
+    public static final String DASH = "-";
+    public static final String POINT = ".";
     public static final int TEST_ID = 0;
     public static final int DATE_ID = 1;
     public static final int MARK_ID = 2;
