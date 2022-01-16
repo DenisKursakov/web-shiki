64cd5fa36ff48b9ef4cf0916e60df846dd7b05cb
DenisKursakov <84324765+DenisKursakov@users.noreply.github.com>
2022-01-16 16:49:16 +0300

    fifth commit

--- src/by/epam/lab/beans/ResultsHandler.java
+++ src/by/epam/lab/beans/ResultsHandler.java
@@ -25,31 +25,6 @@ public class ResultsHandler extends DefaultHandler {
     }
 
     @Override
-    public void setDocumentLocator(Locator locator) {
-
-    }
-
-    @Override
-    public void startDocument() throws SAXException {
-
-    }
-
-    @Override
-    public void endDocument() throws SAXException {
-
-    }
-
-    @Override
-    public void startPrefixMapping(String prefix, String uri) throws SAXException {
-
-    }
-
-    @Override
-    public void endPrefixMapping(String prefix) throws SAXException {
-
-    }
-
-    @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes)
             throws SAXException {
         currentEnum = ResultEnum.valueOf(localName.toUpperCase());
@@ -60,11 +35,6 @@ public class ResultsHandler extends DefaultHandler {
     }
 
     @Override
-    public void endElement(String uri, String localName, String qName) throws SAXException {
-
-    }
-
-    @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
         String currentStr = new String(ch, start, length).trim();
         if (currentEnum == ResultEnum.LOGIN) {
@@ -72,20 +42,5 @@ public class ResultsHandler extends DefaultHandler {
                 currentLogin = currentStr;
             }
         }
-    }
-
-    @Override
-    public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
-
-    }
-
-    @Override
-    public void processingInstruction(String target, String data) throws SAXException {
-
-    }
-
-    @Override
-    public void skippedEntity(String name) throws SAXException {
-
     }
 }
